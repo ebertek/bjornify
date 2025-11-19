8ec9f1415a5127d5ac00561560edb9c2408b5321
eebedav <ebertek@gmail.com>
2025-11-19 12:48:43 +0100

    Lint

--- bjornify/bjornify.py
+++ bjornify/bjornify.py
@@ -54,6 +54,7 @@ os.makedirs(os.path.dirname(LOG_PATH), exist_ok=True)
 # Common formatters
 class JsonFormatter(logging.Formatter):
     """Custom log formatter."""
+
     def format(self, record: logging.LogRecord) -> str:
         log = {
             "timestamp": datetime.fromtimestamp(record.created).isoformat(),
